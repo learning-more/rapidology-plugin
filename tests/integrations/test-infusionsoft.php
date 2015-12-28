83fa5cb1227fab3b717276455c5b51f21e6d5f12
Brandon Braner <brandon.braner@ave81.com>
2015-12-28 09:49:41 -0600

    upgraded to latest version of 3x of rpcxml. Redid some constructors to make them php7 complaint.

---
+++ tests/integrations/test-infusionsoft.php
@@ -0,0 +1,33 @@
+<?php
+
+include(RAD_RAPIDOLOGY_PLUGIN_DIR.'/includes/classes/integrations/class.rapidology-infusionsoft.php');
+
+class IntegrationsTestInfusionsoft extends WP_UnitTestCase {
+
+  function setup() {
+	$path = dirname(dirname(dirname(dirname(dirname(plugin_dir_path(__FILE__))))));
+	include($path . '/testCreds.php');
+
+	$this->instance    = $testCreds->infusionsoft->instance;
+	$this->integration = new $this->instance();
+	$this->apiKey      = $testCreds->infusionsoft->apiKey;
+	$this->app_id       = $testCreds->infusionsoft->appid;
+	$this->name        = $testCreds->infusionsoft->name;
+	$this->badApiKey   = $testCreds->infusionsoft->badApiKey;
+	$this->email       = $testCreds->infusionsoft->email;
+	$this->first_name  = $testCreds->infusionsoft->first_name;
+	$this->last_name   = $testCreds->infusionsoft->last_name;
+	$this->list_id     = $testCreds->infusionsoft->list_id;
+	$this->bademail    = $testCreds->infusionsoft->bademail;
+
+  }
+
+
+	function test_get_infusionsoft_lists_success(){
+	  //ensure object is created and is instance of the correct provider
+	  $this->assertInstanceOf($this->instance, $this->integration);
+	  $result = $this->integration->get_infusionsoft_lists( $this->app_id, $this->apiKey, $this->name );
+	  $expected_result = 'success';
+	  $this->assertEquals($expected_result, $result, $result);
+	}
+}

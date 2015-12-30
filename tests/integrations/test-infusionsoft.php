4973d2491b2516b714a27c7f6835bd40e38a9b7a
Brandon Braner <brandon.braner@ave81.com>
2015-12-30 11:26:15 -0600

    finished infusionsoft tests

--- tests/integrations/test-infusionsoft.php
+++ tests/integrations/test-infusionsoft.php
@@ -32,4 +32,48 @@ class IntegrationsTestInfusionsoft extends WP_UnitTestCase {
 	  $expected_result = 'success';
 	  $this->assertEquals($expected_result, $result, $result);
 	}
+
+  /**
+   * group success
+   */
+  function test_subscribe_infusionsoft_success(){
+	$this->assertInstanceOf($this->instance, $this->integration);
+	$result = $this->integration->subscribe_infusionsoft( $this->apiKey, $this->app_id, $this->list_id, $this->email, $this->first_name, $this->last_name);
+	$expected_result = array('success', 'Already In List');
+	//assert true if either one of these repsonses come back
+	if(in_array($result, $expected_result)){
+	  $this->assertTrue(true);
+	}
+  }
+
+  /**
+   * group fail
+   */
+  function test_get_infusionsoft_lists_fail_bad_key(){
+	//ensure object is created and is instance of the correct provider
+	$this->assertInstanceOf($this->instance, $this->integration);
+	$result = $this->integration->get_infusionsoft_lists( $this->badApiKey, $this->apiKey, $this->name );
+	$expected_result = 'success';
+	$this->assertNotEquals($expected_result, $result, $result);
+  }
+  /**
+   * group fail
+   * @expectedException iSDKException [InvalidKey]Invalid
+   */
+
+  function test_subscribe_infusionsoft_fail_bad_key(){
+	$this->assertInstanceOf($this->instance, $this->integration);
+	$result = $this->integration->subscribe_infusionsoft( $this->badApiKey, $this->app_id, $this->list_id, $this->email, $this->first_name, $this->last_name);
+	//should return iSDKException [InvalidKey]Invalid key  the annotation above asserts that for us
+  }
+
+  /**
+   * group fail
+   */
+  function test_subscribe_infusionsoft_fail_bad_email(){
+	$this->assertInstanceOf($this->instance, $this->integration);
+	$result = $this->integration->subscribe_infusionsoft( $this->apiKey, $this->app_id, $this->list_id, $this->bademail, $this->first_name, $this->last_name);
+	$expected_result = 'success';
+	$this->assertNotEquals($expected_result, $result, $result);
+  }
 }
